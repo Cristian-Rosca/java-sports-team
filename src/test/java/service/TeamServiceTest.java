dccbb1c41b56b30801855f452d490a24cb06d62a
Cristian Rosca <cristianrrosca@gmail.com>
2022-02-07 19:00:47 +0000

    add tests for countEmptySpacesOnTeam method

--- src/test/java/service/TeamServiceTest.java
+++ src/test/java/service/TeamServiceTest.java
@@ -1,13 +1,47 @@
 package service;
 
+import org.junit.jupiter.api.Test;
+import pojo.Manager;
+import pojo.Team;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TeamServiceTest {
 
-    // Given
+    @Test // have to include @Test before writing the test
+    public void canCountEmptySpacesOnTeamWhenEmpty () {
+        // Given
+        Manager manager = new Manager("test Manager");
 
-    // When
+        Team team = new Team("teamA", manager);
 
-    // Then
+        TeamService underTest = new TeamService();
 
+        // When
+        int actual = underTest.countEmptySpacesOnTeam(team);
+
+        // Then
+        int expected = 15;
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void canCountEmptySpacesOnTeamWithOnePlayer () {
+        // Given
+        Manager manager = new Manager("test Manager");
+
+        Team team = new Team("teamA", manager);
+
+        TeamService underTest = new TeamService();
+
+        // When
+        int actual = underTest.countEmptySpacesOnTeam(team);
+
+        // Then
+        int expected = 15;
+        assertThat(actual).isEqualTo(expected);
+
+    }
 }
