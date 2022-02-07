039193bb7ddcde8760ebab2bc756f00d72423c30
Cristian Rosca <cristianrrosca@gmail.com>
2022-02-07 14:17:44 +0000

    add alternate for loop

--- src/main/java/service/TeamService.java
+++ src/main/java/service/TeamService.java
@@ -12,11 +12,21 @@ public class TeamService {
     // parameters : Team
 
     public int countEmptySpacesOnTeam (Team team) {
+//       foreach loop method - this one is easier to set up in this case
+//        int count = 0;
+//        for (Player player : team.getPlayers()) // foreach loop. Can use a fori loop also
+//            if (player == null) {
+//                count++;
+//            }
+
+        // fori method - this looping method has more control 
+        Player [] players = team.getPlayers(); // this one is more useful if we start from end of array, if we want to stop halfway, if we want to increment by 2 and so on
         int count = 0;
-        for (Player player : team.getPlayers()) // foreach loop. Can use a fori loop also
-            if (player == null) {
+        for (int i = 0; i < players.length; i++) {
+            if (players[i] == null) {
                 count++;
             }
+        }
         return count;
     }
 
